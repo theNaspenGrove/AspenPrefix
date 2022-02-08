b5f62c57f3d6b31e091c7dc9db7bdebb19c44f77
mov <mov51@mov51.net>
2022-02-07 22:48:12 -0500

    Add login Listener

---
+++ src/main/java/net/mov51/aspenprefix/listeners/playerLogIn.java
@@ -0,0 +1,14 @@
+package net.mov51.aspenprefix.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import static net.mov51.aspenprefix.helpers.PrefixHelper.loadPlayerPrefixList;
+
+public class playerLogIn implements Listener {
+    @EventHandler
+    public void PlayerJoin(PlayerJoinEvent e) {
+        loadPlayerPrefixList(e.getPlayer());
+    }
+}
